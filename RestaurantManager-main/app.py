154261dcc9246e63fdba9200c599184e4e17da1f
DerDer3 <djmcnee3@gmail.com>
2026-04-14 13:45:18 -0600

    Implemented structure for how basic search will work

--- RestaurantManager-main/app.py
+++ RestaurantManager-main/app.py
@@ -10,36 +10,47 @@ app.teardown_appcontext(close_db)
 @app.route("/")
 def index():
     query = request.args.get("q", "")
+    search = reuest.args.get("search_by", "")
     min_rating = request.args.get("min_rating", 0, type=float)
     results = []
 
     if query:
-        cursor = get_db().cursor(dictionary=True)
-        cursor.execute("""
-            SELECT DISTINCT r.id, r.name, r.location, r.michelin_stars, r.is_q1
-            FROM Restaurant r
-            WHERE r.location LIKE %s
-        """, (f"%{query}%",))
-        restaurants = cursor.fetchall()
+        match search:
+            case "location":
 
-        for restaurant in restaurants:
-            cursor.execute("""
-                SELECT c.name, c.specialty, c.exp
-                FROM Chef c
-                JOIN WorksAt w ON c.id = w.chef_id
-                WHERE w.restaurant_id = %s
-            """, (restaurant["id"],))
-            restaurant["chefs"] = cursor.fetchall()
+                cursor = get_db().cursor(dictionary=True)
+                cursor.execute("""
+                    SELECT DISTINCT r.id, r.name, r.location, r.michelin_stars, r.is_q1
+                    FROM Restaurant r
+                    WHERE r.location LIKE %s
+                """, (f"%{query}%",))
+                restaurants = cursor.fetchall()
 
-            cursor.execute("""
-                SELECT d.name, d.price, d.avg_rating, d.calorie_count
-                FROM Dish d
-                JOIN Serves s ON d.id = s.dish_id
-                WHERE s.restaurant_id = %s
-            """, (restaurant["id"],))
-            restaurant["dishes"] = cursor.fetchall()
+                for restaurant in restaurants:
+                    cursor.execute("""
+                        SELECT c.name, c.specialty, c.exp
+                        FROM Chef c
+                        JOIN WorksAt w ON c.id = w.chef_id
+                        WHERE w.restaurant_id = %s
+                    """, (restaurant["id"],))
+                    restaurant["chefs"] = cursor.fetchall()
 
-            results.append(restaurant)
+                    cursor.execute("""
+                        SELECT d.name, d.price, d.avg_rating, d.calorie_count
+                        FROM Dish d
+                        JOIN Serves s ON d.id = s.dish_id
+                        WHERE s.restaurant_id = %s
+                    """, (restaurant["id"],))
+                    restaurant["dishes"] = cursor.fetchall()
+
+                    results.append(restaurant)
+
+            case "restaurants":
+
+            case "chefs":
+
+            case "dishes":
+
 
     return render_template("index.html", results=results, query=query, min_rating=min_rating)
 
